522b2b63bd0f356b7ca84b612b433743d6bf946a
Marek Królikowski <admin@wset.edu.pl>
2024-06-30 05:09:04 +0200

    Updated to support 8.0 Update 3

--- ghettoVCB.sh
+++ ghettoVCB.sh
@@ -9,7 +9,7 @@
 #                   User Definable Parameters
 ##################################################################
 
-LAST_MODIFIED_DATE=2023_09_29
+LAST_MODIFIED_DATE=2024_06_30
 VERSION=1
 
 # directory that all VM backups should go (e.g. /vmfs/volumes/SAN_LUN1/mybackupdir)
@@ -309,7 +309,7 @@ sanityCheck() {
     ESX_RELEASE=$(uname -r)
 
     case "${ESX_VERSION}" in
-        8.0.0|8.0.1|8.0.2)          VER=8; break;;
+        8.0.0|8.0.1|8.0.2|8.0.3)    VER=8; break;;
         7.0.0|7.0.1|7.0.2|7.0.3)    VER=7; break;;
         6.0.0|6.5.0|6.7.0)          VER=6; break;;
         5.0.0|5.1.0|5.5.0)          VER=5; break;;
